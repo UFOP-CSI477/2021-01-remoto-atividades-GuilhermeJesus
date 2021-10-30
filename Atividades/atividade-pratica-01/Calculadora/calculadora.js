dca46f7dcfaca90e4db62bb708de9ff4fd196ba6
GuilhermeJesus <30881168+GuilhermeJesus@users.noreply.github.com>
2021-10-30 09:30:18 -0300

    update

--- Atividades/atividade-pratica-01/Calculadora/calculadora.js
+++ Atividades/atividade-pratica-01/Calculadora/calculadora.js
@@ -4,23 +4,43 @@ let operador = '';
 let numberString='';
 let numberString2='';
 let cont = 0;
+let resultado=0;
+let tudo=0;
 
 function getInfo(value){
 
-    if(isNaN(value) && value!='.'){
+    if(cont==0 && isNaN(value) && value!='.'){
         operador=value;
         document.getElementById('display-superior').innerHTML = `${number1} ${operador}`;
         cont=cont +1;
         value=0;
         return;
     }
+    /*
+    if(cont == 1 && isNaN(value) && value!='.'){
+        resultado = calcula();
+        document.getElementById('display-superior').innerHTML = `${resultado} ${operador} ${number2} ${value}`;
+        document.getElementById('display').innerHTML = resultado;
+
+        operador= value;
+
+        number1=parseFloat(resultado);
+        number2=0;
+        operador = '';
+        numberString='';
+        numberString2='';
+        cont = 1;
+    }
+    */
+    
+
     if(cont == 0){
         numberString = `${numberString}${value}`;
         number1=numberString;
         document.getElementById('display').innerHTML = number1;
         document.getElementById('display-superior').innerHTML = `${number1}`;
     }
-    if(cont == 1 ){
+    if(cont == 1){
         numberString2 = `${numberString2}${value}`;
         number2=numberString2;
         document.getElementById('display').innerHTML = number2;
@@ -35,6 +55,11 @@ function calcula(){
     if(operador == '+'){
         aux = parseFloat(number1) + parseFloat(number2);
         document.getElementById('display').innerHTML = aux;
+        limpar();
+        document.getElementById('display').innerHTML = aux;
+        document.getElementById('display-superior').innerHTML = aux;
+        number1 = aux;
+        return aux;
     }
     if(operador == '-'){
         aux = parseFloat(number1) - parseFloat(number2);
@@ -51,7 +76,6 @@ function calcula(){
 }
 
 function limpar(){
-    //Falta concertar
     document.getElementById('display').innerHTML = '';
     document.getElementById('display-superior').innerHTML = '';
     number1=0;
@@ -60,5 +84,6 @@ function limpar(){
     numberString='';
     numberString2='';
     cont = 0;
+    resultado =0;
 }
 
