161077724d28465e10488c2bad4bd480c2f667a5
GuilhermeJesus <30881168+GuilhermeJesus@users.noreply.github.com>
2021-10-31 10:51:44 -0300

    update

--- Atividades/atividade-pratica-01/Controle de Pix/pix.js
+++ Atividades/atividade-pratica-01/Controle de Pix/pix.js
@@ -1,3 +1,6 @@
+let saldo = 0;
+//vetor de historico
+
 function limparSelect(campo){
 
     while(campo.length > 1){
@@ -28,4 +31,37 @@ function preencherSelectBancos(data){
         bancos.appendChild(option);
 
     }
+}
+
+function carregarBancos2(){
+
+    fetch("https://brasilapi.com.br/api/banks/v1")
+        .then(response => response.json())
+        .then(data => preencherSelectBancos2(data))
+        .catch(error => console.error(error))
+}
+
+function preencherSelectBancos2(data){
+
+    let bancos2 = document.getElementById("bancos2");
+    limparSelect(bancos2);
+
+    for(let index in data){
+
+        const{name} = data[index];
+
+        let option = document.createElement("option");
+        option.innerHTML = name;
+
+        bancos2.appendChild(option);
+
+    }
+}
+
+function executa(){
+
+    //novo saldo
+
+    //salvar em um vetor para historico
+
 }
