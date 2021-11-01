7bb237ea6ba59da6fd06a443d29e571d541c9479
GuilhermeJesus <30881168+GuilhermeJesus@users.noreply.github.com>
2021-11-01 19:00:15 -0300

    update

--- Atividades/atividade-pratica-01/Controle de Pix/pix.js
+++ Atividades/atividade-pratica-01/Controle de Pix/pix.js
@@ -1,81 +1,74 @@
-let saldo =1000;
+let saldo = 1000;
 cont = 0;
 list = [];
 
-function limparSelect(campo){
-
-    while(campo.length > 1){
-        campo.remove(1);
-    }
+function limparSelect(campo) {
+  while (campo.length > 1) {
+    campo.remove(1);
+  }
 }
 
-function carregarBancos(){
-
-    fetch("https://brasilapi.com.br/api/banks/v1")
-        .then(response => response.json())
-        .then(data => preencherSelectBancos(data))
-        .catch(error => console.error(error))
+function carregarBancos() {
+  fetch("https://brasilapi.com.br/api/banks/v1")
+    .then((response) => response.json())
+    .then((data) => preencherSelectBancos(data))
+    .catch((error) => console.error(error));
 }
 
-function preencherSelectBancos(data){
+function preencherSelectBancos(data) {
+  let bancos = document.getElementById("bancos");
+  limparSelect(bancos);
 
-    let bancos = document.getElementById("bancos");
-    limparSelect(bancos);
+  for (let index in data) {
+    const { name } = data[index];
 
-    for(let index in data){
+    let option = document.createElement("option");
+    option.innerHTML = name;
 
-        const{name} = data[index];
-
-        let option = document.createElement("option");
-        option.innerHTML = name;
-
-        bancos.appendChild(option);
-
-    }
+    bancos.appendChild(option);
+  }
 }
 
-function carregarBancos2(){
-
-    fetch("https://brasilapi.com.br/api/banks/v1")
-        .then(response => response.json())
-        .then(data => preencherSelectBancos2(data))
-        .catch(error => console.error(error))
+function carregarBancos2() {
+  fetch("https://brasilapi.com.br/api/banks/v1")
+    .then((response) => response.json())
+    .then((data) => preencherSelectBancos2(data))
+    .catch((error) => console.error(error));
 }
 
-function preencherSelectBancos2(data){
+function preencherSelectBancos2(data) {
+  let bancos2 = document.getElementById("bancos2");
+  limparSelect(bancos2);
 
-    let bancos2 = document.getElementById("bancos2");
-    limparSelect(bancos2);
+  for (let index in data) {
+    const { name } = data[index];
 
-    for(let index in data){
+    let option = document.createElement("option");
+    option.innerHTML = name;
 
-        const{name} = data[index];
-
-        let option = document.createElement("option");
-        option.innerHTML = name;
-
-        bancos2.appendChild(option);
-
-    }
+    bancos2.appendChild(option);
+  }
 }
 
-function atualizaSaldo(){
-    document.getElementById("txtSaldo").innerHTML = parseFloat(saldo.toPrecision(8));
+function atualizaSaldo() {
+  document.getElementById("txtSaldo").innerHTML = parseFloat(
+    saldo.toPrecision(8)
+  );
 }
 
-function guardaTransacao(){
+function guardaTransacao() {
+  saldo = parseFloat(
+    saldo + parseFloat(document.getElementById("valor").value)
+  );
+  atualizaSaldo(saldo);
 
-    saldo = parseFloat(saldo + parseFloat(document.getElementById('valor').value));
-    atualizaSaldo(saldo);
+  let ind = document.getElementById("indicador").value;
+  let valor = document.getElementById("valor").value;
+  let nome = document.getElementById("nome").value;
+  let b1 = document.getElementById("bancos").value;
+  let b2 = document.getElementById("bancos2").value;
 
-    let ind = document.getElementById('indicador').value;
-    let valor = document.getElementById('valor').value;
-    let nome = document.getElementById('nome').value;
-    let b1 = document.getElementById('bancos').value;
-    let b2 = document.getElementById('bancos2').value;
+  //testando
 
-    //testando
-    list.push(cont);
-    cont++;
-    
+
 }
