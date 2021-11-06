7efffa6e733f759f4ff375410bbafcf5caddcc54
GuilhermeJesus <30881168+GuilhermeJesus@users.noreply.github.com>
2021-11-06 16:25:33 -0300

    update

--- Atividades/atividade-pratica-01/Conversor/conversor.js
+++ Atividades/atividade-pratica-01/Conversor/conversor.js
@@ -1,3 +1,6 @@
+let valor1=0;
+let valor2=0;
+
 function limparSelect(campo) {
   while (campo.length > 1) {
     campo.remove(1);
@@ -46,4 +49,59 @@ function preencherSelectMoeda2(data) {
 
     moeda2.appendChild(option);
   }
+}
+
+// ---------------------------------------------------------
+
+let urlTeste ="https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='EUR'&@dataCotacao='11-05-2021'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda"
+let url2 ="https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='EUR'&@dataCotacao='11-05-2021'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda"
+
+function converte(){
+
+  valor1 = carregar();
+
+  valor2 = carregar2();
+
+  window.alert(valor1);
+  console.log(valor2);
+}
+
+function carregar() {
+  fetch("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='USD'&@dataCotacao='11-05-2021'&$top=100&$format=json&$select=cotacaoCompra")
+    .then((response) => response.json())
+    .then((data) => recebeValor(data.value))
+    .catch((error) => console.error(error));
+}
+
+function recebeValor(data) {
+
+  console.log(data)
+  
+  for(key in data) {
+    if(data.hasOwnProperty(key)) {
+        var cot = Object.values(data[key]);
+    }
+  }
+  console.log(cot)
+return cot;
+}
+
+function carregar2() {
+  fetch("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='USD'&@dataCotacao='11-04-2021'&$top=100&$format=json&$select=cotacaoCompra")
+    .then((response) => response.json())
+    .then((data) => recebeValor2(data.value))
+    .catch((error) => console.error(error));
+}
+
+function recebeValor2(data) {
+
+  console.log(data)
+  
+  for(key in data) {
+    if(data.hasOwnProperty(key)) {
+        var cot = Object.values(data[key]);
+    }
+  }
+  console.log(cot)
+return cot;
 }
