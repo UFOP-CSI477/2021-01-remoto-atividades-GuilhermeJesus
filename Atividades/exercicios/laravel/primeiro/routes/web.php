bc19c503dbd206a1423743289c2b6852a0fd0b3f
Guilherme Jesus <guilherme.garj@hotmail.com>
2021-12-21 16:53:52 -0300

    laravel_route_view

--- Atividades/exercicios/laravel/primeiro/routes/web.php
+++ Atividades/exercicios/laravel/primeiro/routes/web.php
@@ -13,6 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Models\Estado;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/ola', function(){
+    return 'Ola Mundo!';
+});
+
+Route::get('/estados', function(){
+    $estados = Estado::all();
+    return view('lista', ['dados' => $estados]);
+});
+
+Route::get('/estados/{id}', function(){
+    $estado = Estado::find($id);
+
+    if($estado == null){
+        return 'ID INVALIDO';
+    }
+    
+    return view('lista', ['dados' => $estado]);
 });
